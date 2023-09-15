dbaf475946dc2478df701f5f94c1844e72f9426c
ShiNNXD <kaikymassaki2005@gmail.com>
2023-09-15 10:55:41 -0300

    sidebar

---
+++ Sistema/Admin/sidebarAdmin.php
@@ -0,0 +1,56 @@
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="indexAdmin.php">
+                <div class="sidebar-brand-icon rotate-n-15">
+                <i class="fa-solid fa-paw"></i>
+                </div>
+                <div class="sidebar-brand-text mx-1">Clínica Veterinária</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link" href="listagemAgenda.php">
+                    <i class="fa-solid fa-calendar-days"></i>
+                    <span>Agenda</span>
+                </a>
+            </li>
+
+            <!-- Nav Item - Utilities Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link" href="listagemVeterinario.php">
+                <i class="fa-solid fa-user-doctor"></i>
+                    <span>Veterinário</span>
+                </a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Nav Item - Pages Collapse Menu -->
+            
+            <li class="nav-item">
+                <a class="nav-link" href="listagemCliente.php">
+                <i class="fa-solid fa-user"></i>
+                    <span>Cliente</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="listagemPet.php">
+                <i class="fa-solid fa-dog"></i>
+                    <span>Pet</span>
+                </a>
+            </li>
+
+        </ul>
+        <!-- End of Sidebar -->
