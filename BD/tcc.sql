f2dc0e0952db37eb837981bd9a96e95c2add51aa
Kaiky <kaikymassaki2005@gmail.com>
2023-12-04 23:51:54 -0300

    bd

--- BD/tcc.sql
+++ BD/tcc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 01/12/2023 às 19:08
+-- Tempo de geração: 05/12/2023 às 03:50
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -57,15 +57,25 @@ INSERT INTO `admin` (`id`, `status`, `nome`, `telefone`, `email`, `dataNasciment
 
 CREATE TABLE `agenda` (
   `id` int(11) NOT NULL,
-  `status` varchar(10) NOT NULL,
+  `statusAgenda` varchar(20) NOT NULL,
   `data` varchar(50) NOT NULL,
   `hora` varchar(50) NOT NULL,
   `obs` varchar(500) DEFAULT NULL,
-  `resultado` varchar(200) DEFAULT NULL,
+  `resultado` varchar(500) DEFAULT NULL,
   `pet_id` int(11) DEFAULT NULL,
   `veterinario_id` int(11) DEFAULT NULL,
   `procedimento_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `agenda`
+--
+
+INSERT INTO `agenda` (`id`, `statusAgenda`, `data`, `hora`, `obs`, `resultado`, `pet_id`, `veterinario_id`, `procedimento_id`) VALUES
+(1, 'Concluído', '2023-12-02', '09:30', '', 'asdas', 2, 4, 14),
+(2, 'Em Andamento', '2023-12-13', '15:30', '', NULL, 7, 1, 12),
+(3, 'Em Andamento', '2023-12-06', '16:00', '', NULL, 1, 2, 3),
+(4, 'Concluído', '2023-12-02', '09:00', '', NULL, 7, 4, 14);
 
 -- --------------------------------------------------------
 
@@ -75,10 +85,11 @@ CREATE TABLE `agenda` (
 
 CREATE TABLE `atendente` (
   `id` int(11) NOT NULL,
-  `status` varchar(20) NOT NULL,
+  `statusAtendente` varchar(20) NOT NULL,
   `nome` varchar(200) NOT NULL,
   `telefone` varchar(20) NOT NULL,
   `email` varchar(200) NOT NULL,
+  `sexo` char(1) NOT NULL,
   `dataNascimento` date NOT NULL,
   `dataAdmissao` date NOT NULL DEFAULT current_timestamp(),
   `senha` varchar(200) NOT NULL,
@@ -90,8 +101,8 @@ CREATE TABLE `atendente` (
 -- Despejando dados para a tabela `atendente`
 --
 
-INSERT INTO `atendente` (`id`, `status`, `nome`, `telefone`, `email`, `dataNascimento`, `dataAdmissao`, `senha`, `cpf`, `dataDemissao`) VALUES
-(1, '', 'Paulo', '(44) 99718-3800', 'abcde@gmail.com', '2023-11-08', '2023-11-30', '1234567', '082.423.899-00', NULL);
+INSERT INTO `atendente` (`id`, `statusAtendente`, `nome`, `telefone`, `email`, `sexo`, `dataNascimento`, `dataAdmissao`, `senha`, `cpf`, `dataDemissao`) VALUES
+(1, 'Ativo', 'Paulo', '(44) 99718-3800', 'ae@gmail.com', 'M', '2023-11-08', '2023-11-30', '1234567', '093.933.938-26', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -101,7 +112,7 @@ INSERT INTO `atendente` (`id`, `status`, `nome`, `telefone`, `email`, `dataNasci
 
 CREATE TABLE `cliente` (
   `id` int(11) NOT NULL,
-  `status` tinyint(1) NOT NULL,
+  `status` varchar(15) NOT NULL,
   `nome` varchar(200) NOT NULL,
   `telefone` varchar(20) NOT NULL,
   `endereco` varchar(200) NOT NULL,
@@ -111,7 +122,6 @@ CREATE TABLE `cliente` (
   `dataNascimento` varchar(50) NOT NULL,
   `CPF` varchar(14) NOT NULL,
   `email` varchar(200) NOT NULL,
-  `senha` varchar(200) NOT NULL,
   `dataCadastro` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -119,12 +129,12 @@ CREATE TABLE `cliente` (
 -- Despejando dados para a tabela `cliente`
 --
 
-INSERT INTO `cliente` (`id`, `status`, `nome`, `telefone`, `endereco`, `cidade`, `UF`, `sexo`, `dataNascimento`, `CPF`, `email`, `senha`, `dataCadastro`) VALUES
-(2, 0, 'Ana Silva Oliveira', '(11) 98765-4321', 'Rua das Flores, 123', 'São Paulo', 'SP', 'F', '1980-05-11', '123.456.789-00', 'ana.silva@email.com', 'ana123', '2023-09-15'),
-(3, 0, 'Marcos Santos Pereira', '(21) 99999-8888', 'Avenida Principal, 456', 'Rio de Janeiro', 'RJ', 'M', '1982-08-20', '987.654.321-00', 'marcos.santos@email.com', 'marcos123', '2023-09-15'),
-(4, 0, 'Camila Alves Souza', '(31) 5555-1234', 'Rua das Palmeiras, 789', 'Belo Horizonte', 'MG', 'F', '1985-11-15', '456.789.123-00', 'camila.alves@email.com', 'camila123', '2023-09-15'),
-(5, 0, 'Luiz Costa Lima', '(41) 7777-5555', 'Travessa das Pedras, 321', 'Curitiba', 'PR', 'M', '1977-04-03', '234.567.890-00', 'luiz.lima@email.com', 'luiz123', '2023-09-15'),
-(6, 0, 'Marina Gonçalves Ribeiro', '(51) 3333-2222', 'Rua das Árvores, 987', 'Porto Alegre', 'RS', 'F', '1990-02-25', '345.678.901-00', 'marina.ribeiro@email.com', 'marina123', '2023-09-15');
+INSERT INTO `cliente` (`id`, `status`, `nome`, `telefone`, `endereco`, `cidade`, `UF`, `sexo`, `dataNascimento`, `CPF`, `email`, `dataCadastro`) VALUES
+(2, 'Ativo', 'Ana Silva Oliveira', '(11) 98765-4321', 'Rua das Flores, 123', 'São Paulo', 'SP', 'F', '1980-05-11', '123.456.789-00', 'ana.silva@email.com', '2023-09-15'),
+(3, 'Ativo', 'Marcos Santos Pereira', '(21) 99999-8888', 'Avenida Principal, 456', 'Rio de Janeiro', 'RJ', 'M', '1982-08-20', '987.654.321-00', 'marcos.santos@email.com', '2023-09-15'),
+(4, 'Ativo', 'Camila Alves Souza', '(31) 5555-1234', 'Rua das Palmeiras, 789', 'Belo Horizonte', 'MG', 'F', '1985-11-15', '456.789.123-00', 'camila.alves@email.com', '2023-09-15'),
+(5, 'Ativo', 'Luiz Costa Lima', '(41) 7777-5555', 'Travessa das Pedras, 321', 'Curitiba', 'PR', 'M', '1977-04-03', '234.567.890-00', 'luiz.lima@email.com', '2023-09-15'),
+(6, 'Ativo', 'Marina Gonçalves Ribeiro', '(51) 3333-2222', 'Rua das Árvores, 987', 'Porto Alegre', 'RS', 'F', '1990-02-25', '345.678.901-00', 'marina.ribeiro@email.com', '2023-09-15');
 
 -- --------------------------------------------------------
 
@@ -166,31 +176,30 @@ INSERT INTO `pet` (`id`, `status`, `nome`, `especie`, `anoNascimento`, `sexo`, `
 
 CREATE TABLE `procedimento` (
   `id` int(11) NOT NULL,
-  `status` varchar(10) NOT NULL,
+  `statusProcedimento` varchar(10) NOT NULL,
   `nome` varchar(100) NOT NULL,
-  `valor` double(11,2) NOT NULL,
-  `categoria` varchar(50) NOT NULL
+  `valor` double(11,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `procedimento`
 --
 
-INSERT INTO `procedimento` (`id`, `status`, `nome`, `valor`, `categoria`) VALUES
-(2, '', 'Consulta Veterinária de Rotina', 50.35, ''),
-(3, '', 'Vacinação Anual', 30.00, ''),
-(4, '', 'Castração/esterilização', 150.00, ''),
-(5, '', 'Exame de Sangue', 80.00, ''),
-(6, '', 'Radiografia ', 100.00, ''),
-(7, '', 'Limpeza Dentária', 70.00, ''),
-(8, '', 'Tratamento para Pulgas e Carrapatos', 40.00, ''),
-(9, '', 'Microchipagem', 25.00, ''),
-(10, '', 'Consulta de Emergência', 80.20, ''),
-(11, '', 'Ultrassonografia ', 120.00, ''),
-(12, '', 'Tratamento para Verminoses', 20.00, ''),
-(13, '', 'Análise de Fezes', 25.00, ''),
-(14, '', 'Fisioterapia para Animais', 60.00, ''),
-(15, '', 'Acompanhamento de Gravidez', 90.00, '');
+INSERT INTO `procedimento` (`id`, `statusProcedimento`, `nome`, `valor`) VALUES
+(2, 'Ativo', 'Consulta Veterinária de Rotina', 50.00),
+(3, 'Ativo', 'Vacinação Anual', 35.00),
+(4, 'Ativo', 'Castração/esterilização', 150.00),
+(5, 'Ativo', 'Exame de Sangue', 80.00),
+(6, 'Ativo', 'Radiografia ', 100.00),
+(7, 'Ativo', 'Limpeza Dentária', 70.00),
+(8, 'Ativo', 'Tratamento para Pulgas e Carrapatos', 40.00),
+(9, 'Ativo', 'Microchipagem', 25.00),
+(10, 'Ativo', 'Consulta de Emergência', 50.00),
+(11, 'Ativo', 'Ultrassonografia ', 120.00),
+(12, 'Ativo', 'Tratamento para Verminoses', 20.00),
+(13, 'Ativo', 'Análise de Fezes', 25.00),
+(14, 'Ativo', 'Fisioterapia para Animais', 60.00),
+(15, 'Ativo', 'Acompanhamento de Gravidez', 90.00);
 
 -- --------------------------------------------------------
 
@@ -200,7 +209,7 @@ INSERT INTO `procedimento` (`id`, `status`, `nome`, `valor`, `categoria`) VALUES
 
 CREATE TABLE `raca` (
   `id` int(11) NOT NULL,
-  `status` varchar(15) NOT NULL,
+  `statusRaca` varchar(15) NOT NULL,
   `nome` varchar(100) NOT NULL,
   `descricao` varchar(100) DEFAULT NULL,
   `especie` varchar(15) DEFAULT NULL
@@ -210,55 +219,39 @@ CREATE TABLE `raca` (
 -- Despejando dados para a tabela `raca`
 --
 
-INSERT INTO `raca` (`id`, `status`, `nome`, `descricao`, `especie`) VALUES
-(1, '', 'Labrador Retriever', ' ', 'Cachorro'),
-(2, '', 'Pastor Alemão', ' ', 'Cachorro'),
-(3, '', 'Golden Retriever', ' ', 'Cachorro'),
-(4, '', 'Bulldog Inglês', ' ', 'Cachorro'),
-(5, '', 'Beagle', ' ', 'Cachorro'),
-(6, '', 'Poodle', ' ', 'Cachorro'),
-(7, '', 'Boxer', ' ', 'Cachorro'),
-(8, '', 'Dachshund', ' ', 'Cachorro'),
-(9, '', 'Chihuahua', ' ', 'Cachorro'),
-(10, '', 'Shih Tzu', ' ', 'Cachorro'),
-(11, '', 'Pug', ' ', 'Cachorro'),
-(12, '', 'Yorkshire Terrier', ' ', 'Cachorro'),
-(13, '', 'Rottweiler', ' ', 'Cachorro'),
-(14, '', 'Doberman Pinscher', ' ', 'Cachorro'),
-(15, '', 'Border Collie', ' ', 'Cachorro'),
-(16, '', 'Schnauzer Miniatura', ' ', 'Cachorro'),
-(17, '', 'Shetland Sheepdog', ' ', 'Cachorro'),
-(18, '', 'Akita', ' ', 'Cachorro'),
-(19, '', 'Husky Siberiano', ' ', 'Cachorro'),
-(20, '', 'Bulldog Francês', ' ', 'Cachorro'),
-(21, '', 'Cocker Spaniel', ' ', 'Cachorro'),
-(22, '', 'Mastiff', ' ', 'Cachorro'),
-(23, '', 'Dálmata', ' ', 'Cachorro'),
-(24, '', 'Shiba Inu', ' ', 'Cachorro'),
-(25, '', 'Sem Raça', ' ', 'Cachorro'),
-(26, '', 'Siamês', ' ', 'Gato'),
-(27, '', 'Persa', ' ', 'Gato'),
-(28, '', 'Bengal', ' ', 'Gato'),
-(29, '', 'Sphynx', ' ', 'Gato'),
-(30, '', 'Birmanês', ' ', 'Gato'),
-(31, '', 'Abissínio', ' ', 'Gato'),
-(32, '', 'Himalaio', ' ', 'Gato'),
-(33, '', 'Teste', ' ', 'Roedor');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `recebimento`
---
-
-CREATE TABLE `recebimento` (
-  `id` int(11) NOT NULL,
-  `status` varchar(10) NOT NULL,
-  `data` datetime NOT NULL,
-  `valorRecebido` double NOT NULL,
-  `saldoReceber` double NOT NULL,
-  `formaRecebimento` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `raca` (`id`, `statusRaca`, `nome`, `descricao`, `especie`) VALUES
+(1, 'Ativo', 'Labrador Retriever', ' ', 'Roedor'),
+(2, 'Ativo', 'Pastor Alemão', ' ', 'Cachorro'),
+(3, 'Ativo', 'Golden Retriever', ' ', 'Cachorro'),
+(4, 'Ativo', 'Bulldog Inglês', ' ', 'Cachorro'),
+(5, 'Ativo', 'Beagle', ' ', 'Cachorro'),
+(6, 'Ativo', 'Poodle', ' ', 'Cachorro'),
+(7, 'Ativo', 'Boxer', ' ', 'Cachorro'),
+(8, 'Ativo', 'Dachshund', ' ', 'Cachorro'),
+(9, 'Ativo', 'Chihuahua', ' ', 'Cachorro'),
+(10, 'Ativo', 'Shih Tzu', ' ', 'Cachorro'),
+(11, 'Ativo', 'Pug', ' ', 'Cachorro'),
+(12, 'Ativo', 'Yorkshire Terrier', ' ', 'Cachorro'),
+(13, 'Ativo', 'Rottweiler', ' ', 'Cachorro'),
+(14, 'Ativo', 'Doberman Pinscher', ' ', 'Cachorro'),
+(15, 'Ativo', 'Border Collie', ' ', 'Cachorro'),
+(16, 'Ativo', 'Schnauzer Miniatura', ' ', 'Cachorro'),
+(17, 'Ativo', 'Shetland Sheepdog', ' ', 'Cachorro'),
+(18, 'Ativo', 'Akita', ' ', 'Cachorro'),
+(19, 'Ativo', 'Husky Siberiano', ' ', 'Cachorro'),
+(20, 'Ativo', 'Bulldog Francês', ' ', 'Cachorro'),
+(21, 'Ativo', 'Cocker Spaniel', ' ', 'Cachorro'),
+(22, 'Ativo', 'Mastiff', ' ', 'Cachorro'),
+(23, 'Ativo', 'Dálmata', ' ', 'Cachorro'),
+(24, 'Ativo', 'Shiba Inu', ' ', 'Cachorro'),
+(25, 'Ativo', 'Sem Raça', ' ', 'Cachorro'),
+(26, 'Ativo', 'Siamês', ' ', 'Gato'),
+(27, 'Ativo', 'Persa', ' ', 'Gato'),
+(28, 'Ativo', 'Bengal', ' ', 'Gato'),
+(29, 'Ativo', 'Sphynx', ' ', 'Gato'),
+(30, 'Ativo', 'Birmanês', ' ', 'Gato'),
+(31, 'Ativo', 'Abissínio', ' ', 'Gato'),
+(32, 'Ativo', 'Himalaio', ' ', 'Gato');
 
 -- --------------------------------------------------------
 
@@ -285,9 +278,9 @@ CREATE TABLE `veterinario` (
 --
 
 INSERT INTO `veterinario` (`id`, `statusVet`, `nome`, `telefone`, `email`, `sexo`, `dataNascimento`, `dataAdmissao`, `senha`, `CRMV`, `dataDemissao`) VALUES
-(1, 'Inativo', 'Maria da Silva', ' (11) 98765-4321', 'maria.silva@email.com', 'F', '1985-03-15', '2023-09-15', 'maria123', '12345-SP', '2023-11-28'),
-(2, 'Ativo', 'João dos Santos', '(21) 99999-8888', 'joao.santos@email.com', 'M', '1990-07-10', '2023-09-15', 'joao123', '67890-RJ', NULL),
-(3, 'Ativo', 'Ana Oliveira', '(31) 5555-1234', 'ana.oliveira@email.com', 'F', '1982-09-25', '2023-09-15', 'ana123', '54321-MG', NULL),
+(1, 'Ativo', 'Paulo', '(44) 99718-3800', 'abcde@gmail.com', 'M', '2023-11-08', '2023-09-15', '1234567', '12456-PR', '0000-00-00'),
+(2, 'Ativo', 'João dos Santos', '(65) 99854-3301', 'joao.santos@email.com', 'M', '1990-07-10', '2023-09-15', 'joao123', '67890-RJ', '0000-00-00'),
+(3, 'Ativo', 'Ana Oliveira', ' (31) 5555-1234', 'ana.oliveira@email.com', 'F', '1982-09-25', '2023-09-15', 'ana123', '54321-MG', '0000-00-00'),
 (4, 'Ativo', 'Pedro Pereira', '(41) 7777-5555', 'pedro.pereira@email.com', 'M', '1978-12-05', '2023-09-15', 'pedro123', '98765-PR', NULL);
 
 --
@@ -343,12 +336,6 @@ ALTER TABLE `raca`
   ADD KEY `fk_especie_raca` (`especie`);
 
 --
--- Índices de tabela `recebimento`
---
-ALTER TABLE `recebimento`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Índices de tabela `veterinario`
 --
 ALTER TABLE `veterinario`
@@ -368,7 +355,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT de tabela `agenda`
 --
 ALTER TABLE `agenda`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `atendente`
@@ -399,12 +386,6 @@ ALTER TABLE `procedimento`
 --
 ALTER TABLE `raca`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
-
---
--- AUTO_INCREMENT de tabela `recebimento`
---
-ALTER TABLE `recebimento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `veterinario`
