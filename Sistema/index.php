e88f1184e00c8098d1ffc9196c1f370fde36baf9
João Gabriel <113688281+TrintaEDois@users.noreply.github.com>
2023-12-05 17:51:36 -0300

    Add files via upload

--- Sistema/index.php
+++ Sistema/index.php
@@ -30,7 +30,7 @@
 <body>
 	<!-- Login -->
 	<div class="limiter">
-		<div class="container-login100" style="background-image: url('login/images/bg-01.jpg');">
+		<div class="container-login100" style="background-image: url('login/images/bg-01.png');">
 			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
 				<form class="login100-form validate-form" action="autenticacao.php" method="post">
 					<span class="login100-form-title p-b-49">
